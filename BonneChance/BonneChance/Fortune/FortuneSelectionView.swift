a68e26146f7b4caed114c81852267b8979870825
junsumun <jn218201@dal.ca>
2024-03-29 20:20:51 -0300

    Changed range of the onboarding steps indicator

--- BonneChance/BonneChance/Fortune/FortuneSelectionView.swift
+++ BonneChance/BonneChance/Fortune/FortuneSelectionView.swift
@@ -9,13 +9,38 @@ import SwiftUI
 
 struct FortuneSelectionView: View {
 
+    @AppStorage("firstName") var currentUserFirstName: String?
+    @AppStorage("lastName") var currentUserLastName: String?
+    @AppStorage("gender") var currentUserGender: Gender?
+    @AppStorage("birthdate") var currentUserBirthdate: String?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
         NavigationStack {
-            NavigationLink(destination: SettingsView()) {
-                Text("test")
+            VStack {
+                Text("User profile")
+                HStack {
+                    Text("First name:")
+                    Text(currentUserFirstName ?? "User first name")
+                }
+                HStack {
+                    Text("Last name:")
+                    Text(currentUserLastName ?? "User last name")
+                }
+                HStack {
+                    Text("Gender:")
+                    Text(currentUserGender?.rawValue ?? Gender.male.rawValue)
+                }
+                HStack {
+                    Text("Birthdate:")
+                    Text(currentUserBirthdate ?? "1234")
+                }
+                
+                NavigationLink(destination: SettingsView()) {
+                    Text("test")
+                }
             }
         }
+        
         
     }
 }
