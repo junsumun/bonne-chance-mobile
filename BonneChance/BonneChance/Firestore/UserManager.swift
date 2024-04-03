b24a0f8429bc935338c5c355d6a40f9a2a9ff211
junsumun <jn218201@dal.ca>
2024-04-03 05:37:51 -0300

    Refactored UserManager code cleaner by using Codable

--- BonneChance/BonneChance/Firestore/UserManager.swift
+++ BonneChance/BonneChance/Firestore/UserManager.swift
@@ -9,11 +9,20 @@ import Foundation
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 
-struct DBUser {
+struct DBUser: Codable {
     let userId: String
     let email: String?
     let photoUrl: String?
     let dateCreated: Date?
+    let isPremium: Bool?
+    
+    init(auth: AuthDataResultModel) {
+        self.userId = auth.uid
+        self.email = auth.email
+        self.photoUrl = auth.photoUrl
+        self.dateCreated = Date()
+        self.isPremium = false
+    }
 }
 
 final class UserManager {
@@ -21,6 +30,26 @@ final class UserManager {
     static let shared = UserManager()
     private init() { }
     
+    private let userCollection = Firestore.firestore().collection("users")
+    
+    private func userDocument(userId: String) async throws -> DocumentReference {
+        userCollection.document(userId)
+    }
+    
+    private let encoder: Firestore.Encoder = {
+        let encoder = Firestore.Encoder()
+        encoder.keyEncodingStrategy = .convertToSnakeCase
+        return encoder
+    }()
+    
+    private let decoder: Firestore.Decoder = {
+        let decoder = Firestore.Decoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }()
+    func createNewUser(user: DBUser) async throws {
+        try await userDocument(userId: user.userId).setData(from: user, merge: false, encoder: encoder)
+    }
     
     func createNewUser(auth: AuthDataResultModel) async throws {
         var userData: [String:Any] = [
@@ -35,21 +64,26 @@ final class UserManager {
             userData["photo_url"] = photoUrl
         }
         
-        try await Firestore.firestore().collection("users").document(auth.uid).setData(userData, merge: false)
+        try await userDocument(userId: auth.uid).setData(userData, merge: false)
     }
     
     func getUser(userId: String) async throws -> DBUser {
-        let snapshot = try await Firestore.firestore().collection("users").document(userId).getDocument()
-        
-        guard let data = snapshot.data(), let userId = data["user_id"] as? String else {
-            throw URLError(.badServerResponse)
-        }
-        
-        let email = data["email"] as? String
-        let photoUrl = data["photo_url"] as? String
-        let dateCreated = data["date_created"] as? Date
-        
-        return DBUser(userId: userId, email: email, photoUrl: photoUrl, dateCreated: dateCreated)
+        try await userDocument(userId: userId).getDocument(as: DBUser.self, decoder: decoder)
     }
+    
+    // Above method is refactored version by using Codable
+//    func getUser(userId: String) async throws -> DBUser {
+//        let snapshot = try await userDocument(userId: userId).getDocument()
+//        
+//        guard let data = snapshot.data(), let userId = data["user_id"] as? String else {
+//            throw URLError(.badServerResponse)
+//        }
+//        
+//        let email = data["email"] as? String
+//        let photoUrl = data["photo_url"] as? String
+//        let dateCreated = data["date_created"] as? Date
+//        
+//        return DBUser(userId: userId, email: email, photoUrl: photoUrl, dateCreated: dateCreated)
+//    }
     
 }
