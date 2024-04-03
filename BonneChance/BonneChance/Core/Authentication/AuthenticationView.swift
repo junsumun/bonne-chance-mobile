e4da33f3f0e7d131179e6470c7340c70bcac6f91
junsumun <jn218201@dal.ca>
2024-04-03 02:37:51 -0300

    Implemented Firestore data fetching to fetch user information

--- BonneChance/BonneChance/Core/Authentication/AuthenticationView.swift
+++ BonneChance/BonneChance/Core/Authentication/AuthenticationView.swift
@@ -9,40 +9,6 @@ import SwiftUI
 import GoogleSignIn
 import GoogleSignInSwift
 
-@MainActor
-final class AuthenticationViewModel: ObservableObject {
-    
-    @Published var didSignInWithApple: Bool = false
-    let signInAppleHelper = SignInAppleHelper()
-    
-    func signInGoogle() async throws {
-        let helper = SignInGoogleHelper()
-        let tokens = try await helper.signIn()
-        
-        try await AuthenticationManager.shared.signInWithGoogle(tokens: tokens)
-    }
-    
-    func signInApple() async throws {
-        signInAppleHelper.startSignInWithAppleFlow { result in
-            switch result {
-            case .success(let signInAppleResult):
-                Task {
-                    do {
-                        try await AuthenticationManager.shared.signInWithApple(tokens: signInAppleResult)
-                        self.didSignInWithApple = true
-                    } catch {
-                        
-                    }
-                }
-            case .failure(let error):
-                print(error)
-            }
-        }
-    }
-    
-    
-}
-
 struct AuthenticationView: View {
     
     @StateObject private var viewModel = AuthenticationViewModel()
---
+++ BonneChance/BonneChance/Core/Authentication/AuthenticationViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  AuthenticationViewModel.swift
+//  BonneChance
+//
+//  Created by Junsu Mun on 2024-03-30.
+//
+
+import Foundation
+
+@MainActor
+final class AuthenticationViewModel: ObservableObject {
+    
+    @Published var didSignInWithApple: Bool = false
+    let signInAppleHelper = SignInAppleHelper()
+    
+    func signInGoogle() async throws {
+        let helper = SignInGoogleHelper()
+        let tokens = try await helper.signIn()
+        
+        let authDataResult = try await AuthenticationManager.shared.signInWithGoogle(tokens: tokens)
+        try await UserManager.shared.createNewUser(auth: authDataResult)
+    }
+    
+    func signInApple() async throws {
+        signInAppleHelper.startSignInWithAppleFlow { result in
+            switch result {
+            case .success(let signInAppleResult):
+                Task {
+                    do {
+                        let authDataResult = try await AuthenticationManager.shared.signInWithApple(tokens: signInAppleResult)
+                        try await UserManager.shared.createNewUser(auth: authDataResult)
+                        self.didSignInWithApple = true
+                    } catch {
+                        
+                    }
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
