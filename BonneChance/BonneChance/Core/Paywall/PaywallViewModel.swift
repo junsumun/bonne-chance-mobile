b1aa1776496fb0e1c73d8909261f85bccacd281d
junsumun <jn218201@dal.ca>
2024-04-17 11:09:56 -0300

    Moved Gender enum to Firestore folder

---
+++ BonneChance/BonneChance/Core/Paywall/PaywallViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  PaywallViewModel.swift
+//  BonneChance
+//
+//  Created by Junsu Mun on 2024-04-16.
+//
+
+import SwiftUI
+
+struct PaywallViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaywallViewModel()
+}
---
+++ BonneChance/BonneChance/shared/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  BonneChance
+//
+//  Created by Junsu Mun on 2024-04-16.
+//
+
+import Foundation
